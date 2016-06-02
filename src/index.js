ea97cbb6da161a113cc477cf340f8f17da07bf4e
oyvindhermansen <oyvind@ludensreklame.no>
2016-06-02 21:43:00 +0200

    made function do export default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-export const heightify = (element) => {
+export default function heightify(element) {
   if (!element) throw new Error('You need to set an element')
   const el = document.querySelectorAll(element)
   const storedHeights = []
@@ -10,10 +10,10 @@ export const heightify = (element) => {
     storedHeights.push(el[i].clientHeight)
   }
 
-  largest = Math.max.apply(Math, storedHeights)
+  storedHeights.map((number, index) => {
+    largest = Math.max.apply(Math, storedHeights)
+    el[index].style.height = `${largest}px`
+  })
 
-  for (let j = 0; j < el.length; j++) {
-    el[j].style.height = `${largest}px`
-  }
   return largest
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,21 @@
 import { expect } from 'chai'
-import { heightify } from '../src/index'
+import heightify from '../src/index'
 
 /* tests are comming */
+
+describe('Heightify', () => {
+  it('should return largest number in array', () => {
+    /*
+    const div = document.createElement('div')
+    const t = document.createTextNode('hello')
+    div.appendChild(t)
+    document.body.appendChild(div)
+    //div.style.height = '100px'
+
+    const h = heightify(div)
+    console.log(h)
+
+    //expect(h).to.equal(200)
+    */
+  })
+})
