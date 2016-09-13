d0ee9dc4606d5bbaea69a87b073971ec31a39834
oyvindhermansen <oyvind@ludensreklame.no>
2016-09-13 08:55:22 +0200

    added test for utils, and all passes

--- test/heightify.test.js
+++ test/heightify.test.js
@@ -1,10 +1,27 @@
 import { expect, assert } from 'chai'
 import { heightify } from '../src/heightify'
+import isObject from '../src/utils/isObject'
+import initialError from '../src/utils/initialError'
 
 describe('Heightify', () => {
 
-  describe('error messages', () => {
+  describe('Utils', () => {
+    describe('isObject', () => {
+      it('should return true if object is passed, and false otherwise', () => {
+        const obj = {}
+        const array = []
+        expect(isObject(obj)).to.be.true
+        expect(isObject(array)).to.be.false
+      })
+    })
+    describe('initialError', () => {
+      it('should throw if heightify is not passed with an object as argument', () => {
+        expect(initialError).to.throw(Error)
+      })
+    })
+  })
 
+  describe('error messages', () => {
     it('should be ok if element is defined', () => {
       const heightifyWithDOMElement = heightify({
         element: 'div'
@@ -21,18 +38,11 @@ describe('Heightify', () => {
     it('should throw when element is not defined', () => {
       expect(() => {
         heightify({})
-      }).to.throw('Heightify requires a DOM node to match the height with.')
-    })
-
-    it('should throw when element is not a string', () => {
-      expect(() => {
-        heightify({element: ['div']})
-      }).to.throw('\'div\' should be a type of string.')
+      }).to.throw('Heightify requires a DOM node to match the height with. Please specify with the object key: \'element\'.')
     })
   })
 
   describe('Main functionality', () => {
-
     it('should find the tallest number in array', () => {
       const h = heightify({element: 'div', hasImages: true})
       const fakeArray = [100, 400, 1000, 2, 5, 200, 232]
